2baaf845e3c9f73acbed688efdbef22e5fbba303
Henrique Sato Pereira <hsp@vortx.com.br>
2022-03-16 20:13:04 -0300

    feat: add Herbarium to create REST endpoints
    Routes were created using custom files and repetitive writing. With this feature, there will be a
    faster alternative to do this same route path as herbs2rest will use herbarium to generate the
    endpoints. In addition, a test code was implemented to validate this function.

---
+++ src/generateRoutesWithHerbarium.js
@@ -0,0 +1,41 @@
+const generateRoutes = require('./generateRoutes')
+
+function findEntitiesAndGroups(herbarium) {
+    const items = Array.from(herbarium.usecases.all.values()).map(e => ({ id: e.entity, group: e.group }))
+    const distinctItems = items.filter(({ entity, group }, index, self) =>
+        self.findIndex(e => e.entity === entity && e.group === group) === index
+    )
+    return distinctItems
+}
+
+function findUsecases(herbarium, entity) {
+    const usecases = herbarium.usecases
+    const getAll = usecases.findBy({ entity: entity, operation: herbarium.crud.read })[0].usecase  // herbarium.crud.readAll
+    const getById = usecases.findBy({ entity: entity, operation: herbarium.crud.read })[0].usecase
+    const post = usecases.findBy({ entity: entity, operation: herbarium.crud.create })[0].usecase
+    const put = usecases.findBy({ entity: entity, operation: herbarium.crud.update })[0].usecase
+    const del = usecases.findBy({ entity: entity, operation: herbarium.crud.delete })[0].usecase
+    return { getAll, getById, post, put, del }
+}
+
+function getControllers(herbarium){
+    const entities = findEntitiesAndGroups(herbarium)
+
+    const controllers = entities.map(entity => {
+        const ucList = findUsecases(herbarium, entity.id)
+        return {
+            name: entity.group,
+            getAll: { usecase: ucList.getAll },
+            getById: { usecase: ucList.getById, id: 'ids' },
+            post: { usecase: ucList.post },
+            put: { usecase: ucList.put },
+            delete: { usecase: ucList.del }
+        }
+    })
+    return controllers
+}
+
+module.exports = (herbarium, routes, showEndpoints) => {
+    const controllers = getControllers(herbarium)
+    generateRoutes(controllers, routes, showEndpoints)
+}
---
+++ test/generateRoutesWithHerbarium.test.js
@@ -0,0 +1,141 @@
+const assert = require('assert')
+const request = require('supertest')
+
+const { Ok, step, usecase } = require('@herbsjs/buchu')
+const express = require('express')
+
+const { herbarium } = require('@herbsjs/herbarium')
+const { entity, field } = require('@herbsjs/herbs')
+const generateRoutesWithHerbarium = require('../src/generateRoutesWithHerbarium')
+
+describe('Herbs2Rest - Generate Routes With Herbarium', () => {
+  const usecaseTest = () => {
+    herbarium.requireAll({})
+
+    const Test =
+      entity('Test', {
+        id: field(Number)
+      })
+
+    herbarium.entities
+      .add(Test, 'Test')
+
+    const read = () => usecase('Read usecase', {
+      request: {},
+      authorize: async _ => Ok(),
+      'Test step': step(_ => Ok())
+    })
+    const create = () => usecase('Create usecase', {
+      request: {},
+      authorize: async _ => Ok(),
+      'Test step': step(_ => Ok())
+    })
+    const update = () => usecase('Update usecase', {
+      request: {},
+      authorize: async _ => Ok(),
+      'Test step': step(_ => Ok())
+    })
+    const deleteuc = () => usecase('Delete usecase', {
+      request: {},
+      authorize: async _ => Ok(),
+      'Test step': step(_ => Ok())
+    })
+
+    herbarium.usecases
+      .add(read, 'ReadUsecase')
+      .metadata({ group: 'Test', operation: herbarium.crud.read, entity: Test })
+
+    herbarium.usecases
+      .add(create, 'CreateUsecase')
+      .metadata({ group: 'Test', operation: herbarium.crud.create, entity: Test })
+
+    herbarium.usecases
+      .add(update, 'UpdateUsecase')
+      .metadata({ group: 'Test', operation: herbarium.crud.update, entity: Test })
+
+    herbarium.usecases
+      .add(deleteuc, 'DeleteUsecase')
+      .metadata({ group: 'Test', operation: herbarium.crud.delete, entity: Test })
+  }
+
+  usecaseTest()
+
+  it('Should resolve and create a get all route', (done) => {
+    // Given
+    const app = express()
+    const routes = new express.Router()
+
+
+    // When
+    generateRoutesWithHerbarium(herbarium, routes, true)
+
+    // Then
+    request(app.use(routes))
+      .get('/test')
+      .expect(200, done)
+  })
+
+  it('Should resolve and create a get by id route', (done) => {
+    // Given
+    const app = express()
+    const routes = new express.Router()
+
+
+    // When
+    generateRoutesWithHerbarium(herbarium, routes, true)
+
+    // Then
+    request(app.use(routes))
+      .get('/test/1')
+      .expect(200, done)
+  }),
+
+    it('Should resolve and create a post route', (done) => {
+      // Given
+      const app = express()
+      const routes = new express.Router()
+
+
+      // When
+      generateRoutesWithHerbarium(herbarium, routes, true)
+
+      // Then
+      request(app.use(routes))
+        .post('/test')
+        .expect(200, done)
+    }),
+
+    it('Should resolve and create a put route', (done) => {
+      // Given
+      const app = express()
+      const routes = new express.Router()
+
+
+      // When
+      generateRoutesWithHerbarium(herbarium, routes, true)
+
+      // Then
+      request(app.use(routes))
+        .put('/test/1')
+        .expect(200, done)
+    }),
+
+    it('Should resolve and create a delete route', (done) => {
+      // Given
+      const app = express()
+      const routes = new express.Router()
+
+
+      // When
+      generateRoutesWithHerbarium(herbarium, routes, true)
+
+      // Then
+      request(app.use(routes))
+        .del('/test/1')
+        .expect(200, done)
+    })
+
+  it('Should throw a JavascriptError if controllersList is null', () => {
+    assert.rejects(() => generateRoutesWithHerbarium(null, routes))
+  })
+})
