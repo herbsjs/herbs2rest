c97fa52a1728078a8175e75b17fa91d912606183
Anderson-Costa-Vx <108097104+Anderson-Costa-Vx@users.noreply.github.com>
2022-07-01 17:37:52 -0300

    feat(herbs2rest): using Herbarium to create REST endpoints
    #22

---
+++ src/generateRoutesWithHerbarium.js
@@ -0,0 +1,45 @@
+const generateRoutesOriginal = require('./generateRoutes')
+
+function findEntitiesAndGroups(herbarium) {
+    const items = Array.from(herbarium.usecases.all.values()).map(usecaseItem =>
+        ({ id: usecaseItem.entity, group: usecaseItem.group })
+    )
+    const distinctItems = items.filter(({ entity, group }, index, self) =>
+        self.findIndex(usecaseItem => usecaseItem.entity === entity && usecaseItem.group === group) === index
+    )
+    return distinctItems
+}
+
+function findUsecases(herbarium, entity) {
+    const usecases = herbarium.usecases
+    const getAll = usecases.findBy({ entity: entity, operation: herbarium.crud.read })[0].usecase
+    const getById = usecases.findBy({ entity: entity, operation: herbarium.crud.read })[0].usecase
+    const post = usecases.findBy({ entity: entity, operation: herbarium.crud.create })[0].usecase
+    const put = usecases.findBy({ entity: entity, operation: herbarium.crud.update })[0].usecase
+    const del = usecases.findBy({ entity: entity, operation: herbarium.crud.delete })[0].usecase
+    return { getAll, getById, post, put, del }
+}
+
+function generateControllers(herbarium) {
+    const entities = findEntitiesAndGroups(herbarium)
+
+    const controllers = entities.map(entity => {
+        const ucList = findUsecases(herbarium, entity.id)
+        return {
+            name: entity.group,
+            getAll: { usecase: ucList.getAll },
+            getById: { usecase: ucList.getById, id: 'ids' },
+            post: { usecase: ucList.post },
+            put: { usecase: ucList.put },
+            delete: { usecase: ucList.del }
+        }
+    })
+    return controllers
+}
+
+function generateRoutes(herbarium, routes, showEndpoints) {
+    const controllers = generateControllers(herbarium)
+    generateRoutesOriginal(controllers, routes, showEndpoints)
+}
+
+module.exports = {generateControllers, generateRoutes}
